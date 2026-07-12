de0e75dc33cdd3300343c0ddc08433ae17cb385e
meowneko123 <3251808715@qq.com>
2026-07-12 22:31:42 +0800

    v1.0.0 主更新

---
+++ Serilog.Sinks.Scpsl/ScpslSink.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Formatting.Display;
+
+namespace Serilog.Sinks.Scpsl
+{
+    /// <summary>
+    /// 代表游戏SCP:SL（SCP:秘密实验室）接入Serilog的Sink。
+    /// </summary>
+    /// <remarks>
+    /// 按照自定义的<see cref="ITextFormatter"/>模板新建此Sink。
+    /// </remarks>
+    /// <param name="formatter">要被传入的自定义模板。</param>
+    public class ScpslSink(ITextFormatter formatter) : ILogEventSink
+    {
+        private readonly ITextFormatter _formatter = formatter ?? new MessageTemplateTextFormatter(
+                "[{Level}] {Message:lj}{NewLine}{Exception}",
+                formatProvider: null);
+        private readonly object _locker = new();
+        private readonly string _prefix = "[Serilog]";
+
+        /// <summary>
+        /// 按照默认<see cref="ITextFormatter"/>模板新建此Sink，新模板遵守以下格式。
+        /// <para><c>[{Level}] {Message:lj}{NewLine}{Exception}</c></para>
+        /// </summary>
+        public ScpslSink() : this(null)
+        {
+
+        }
+        /// <summary>
+        /// 按照自定义的<see cref="ITextFormatter"/>模板与自定义的Prefix头新建此Sink。
+        /// </summary>
+        /// <param name="formatter">要被传入的自定义模板。</param>
+        /// <param name="prefix">自定义的Prefix头。</param>
+        public ScpslSink(ITextFormatter formatter, string prefix) : this(formatter)
+        {
+            _prefix = prefix;
+        }
+        /// <inheritdoc/>
+        public void Emit(LogEvent logEvent)
+        {
+            if (logEvent == null)
+                throw new ArgumentNullException(nameof(logEvent));
+
+            string renderedMessage;
+            lock (_locker)
+            {
+                using var writer = new StringWriter();
+                _formatter.Format(logEvent, writer);
+                renderedMessage = writer.ToString();
+            }
+
+            string prefixPart = string.IsNullOrEmpty(_prefix) ? "" : _prefix + " ";
+            string finalMessage = prefixPart + renderedMessage;
+
+            ServerConsole.AddLog(finalMessage, GetColor(logEvent.Level));
+        }
+
+        private static ConsoleColor GetColor(LogEventLevel level) => level switch
+        {
+            LogEventLevel.Fatal => ConsoleColor.DarkRed,
+            LogEventLevel.Verbose => ConsoleColor.DarkGreen,
+            LogEventLevel.Debug => ConsoleColor.DarkGray,
+            LogEventLevel.Information => ConsoleColor.Cyan,
+            LogEventLevel.Warning => ConsoleColor.Yellow,
+            LogEventLevel.Error => ConsoleColor.Red,
+            _ => ConsoleColor.Gray
+        };
+    }
+}
---
+++ Serilog.Sinks.Scpsl/ScpslSinkExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Events;
+using Serilog.Formatting;
+
+namespace Serilog.Sinks.Scpsl
+{
+    /// <summary>
+    /// 接入Serilog的拓展类合集。
+    /// </summary>
+    public static class ScpslSinkExtensions
+    {
+        /// <summary>
+        /// 添加 Scpsl-LA 输出 Sink。
+        /// </summary>
+        /// <param name="loggerConfiguration">日志配置。</param>
+        /// <param name="formatter">可选的自定义格式化器。</param>
+        /// <param name="restrictedToMinimumLevel">最小日志级别（默认 Verbose）。</param>
+        /// <returns>配置对象。</returns>
+        public static LoggerConfiguration Scpsl(
+            this LoggerSinkConfiguration loggerConfiguration,
+            ITextFormatter formatter = null,
+            LogEventLevel restrictedToMinimumLevel = LogEventLevel.Verbose)
+        {
+            if (loggerConfiguration == null)
+                throw new ArgumentNullException(nameof(loggerConfiguration));
+
+            return loggerConfiguration.Sink(
+                new ScpslSink(formatter),
+                restrictedToMinimumLevel);
+        }
+    }
+}
